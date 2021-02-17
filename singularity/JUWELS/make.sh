62135dd0d9ff5012eb48ca5707e3c276890b5d3f
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-02-17 23:53:15 +0100

    Cleaned up dependencies

--- singularity/JUWELS/make.sh
+++ singularity/JUWELS/make.sh
@@ -1,5 +1,6 @@
 # Build all base images for NEMO
 
-sudo singularity build mpibase.sif openmpi-4.1.0_fedora-32.def
-sudo singularity build io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
-sudo singularity build mpibench.sif ../mpibench.def
+singularity build -F mpibase.sif openmpi-4.1.0_fedora-32.def
+singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
+singularity build -F mpibench.sif ../mpibench.def
+sudo singularity build -F muspectre.sif ../muspectre.def
--- singularity/NEMO/make.sh
+++ singularity/NEMO/make.sh
@@ -1,5 +1,6 @@
 # Build all base images for NEMO
 
-sudo singularity build mpibase.sif openmpi-4.0.2_psm2-11.2.185_gcc-7_ubuntu-18.def
-sudo singularity build io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
-sudo singularity build mpibench.sif ../mpibench.def
+sudo singularity build -F mpibase.sif openmpi-4.0.2_psm2-11.2.185_gcc-7_ubuntu-18.def
+sudo singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
+sudo singularity build -F mpibench.sif ../mpibench.def
+sudo singularity build -F muspectre.sif ../muspectre.def
