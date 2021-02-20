3bf729d4b88729f262e7e7f2df5d50e9c13047e8
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-02-21 00:18:18 +0100

    Need to add Fortran compiler, otherwise parallel NetCDF does not compile

--- singularity/NEMO/make.sh
+++ singularity/NEMO/make.sh
@@ -1,6 +1,5 @@
 # Build all base images for NEMO
 
-sudo singularity build -F mpibase.sif openmpi-4.0.2_psm2-11.2.185.def
-sudo singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
-sudo singularity build -F mpibench.sif ../mpibench.def
-sudo singularity build -F muspectre.sif ../muspectre.def
+singularity build -F mpibase.sif openmpi-4.0.2_psm2-11.2.185.def
+singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
+singularity build -F muspectre.sif ../muspectre.def
