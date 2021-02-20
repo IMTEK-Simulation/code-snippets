ad1856b9d3422a52510f924c067c6c368f665f00
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-02-20 21:53:48 +0100

    First version of a container that works on JUWELS; some cleanup

--- singularity/JUWELS/make.sh
+++ singularity/JUWELS/make.sh
@@ -1,6 +1,5 @@
 # Build all base images for NEMO
 
-singularity build -F mpibase.sif openmpi-4.1.0_fedora-32.def
+singularity build -F mpibase.sif openmpi-4.1.0rc1_ucx-1.8.1.def
 singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
-singularity build -F mpibench.sif ../mpibench.def
-sudo singularity build -F muspectre.sif ../muspectre.def
+singularity build -F muspectre.sif ../muspectre.def
