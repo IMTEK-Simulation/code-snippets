9cc97896b6339307d2534fbc75ce9015914e231a
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-02-21 13:08:02 +0100

    Remove directories before compiling (can otherwise cause conflicts from a previous install)

--- singularity/JUWELS/make.sh
+++ singularity/JUWELS/make.sh
@@ -1,5 +1,5 @@
 # Build all base images for NEMO
 
 singularity build -F mpibase.sif openmpi-4.1.0rc1_ucx-1.8.1.def
-singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
+singularity build -F fftw_and_io.si ../pnetcdf-1.12.2_fftw-3.3.9.def
 singularity build -F muspectre.sif ../muspectre.def
--- singularity/NEMO/make.sh
+++ singularity/NEMO/make.sh
@@ -1,5 +1,5 @@
 # Build all base images for NEMO
 
 singularity build -F mpibase.sif openmpi-4.0.2_psm2-11.2.185.def
-singularity build -F io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
+singularity build -F fftw_and_io.sif ../pnetcdf-1.12.2_fftw-3.3.9.def
 singularity build -F muspectre.sif ../muspectre.def
