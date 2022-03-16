7306ef00d930f43852973b73a2e9c1db7c876d7d
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2022-03-16 15:06:11 +0100

    BUG: don'T use generate manifest

--- imteksimcs/dtool/read.py
+++ imteksimcs/dtool/read.py
@@ -2,7 +2,7 @@
 
 
 def get_item_abspath(dataset, relpath):
-    for identifier, properties in dataset.generate_manifest()["items"].items():
-        if properties["relpath"] == relpath:
+    for identifier in dataset.identifiers:
+        if dataset._manifest['items'][identifier]["relpath"] == relpath:
             return dataset.item_content_abspath(identifier)
     return False
