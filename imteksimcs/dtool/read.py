0ca3d6ba9ebe2ce5a9520020eba9d48a3e797502
sannant <antoine.sanner@imtek.uni-freiburg.de>
2022-11-09 09:12:04 +0100

    ENH: utility to list dataset items with relpath

--- imteksimcs/dtool/read.py
+++ imteksimcs/dtool/read.py
@@ -1,6 +1,22 @@
 
 
 
+def list_items(dataset, pattern=None):
+    r'''
+    Parameters:
+    -----------
+    dataset: dtoolcore.DataSet
+    pattern: string
+    show only items with relpath matching the pattern
+    '''
+    item_relpaths = []
+    for key, item in dataset._manifest['items'].items():
+        relpath = dataset._manifest['items'][key]['relpath']
+        if pattern and not pattern in relpath:
+            continue
+        item_relpaths.append(relpath)
+    return item_relpaths
+
 def get_item_abspath(dataset, relpath):
     for identifier in dataset.identifiers:
         if dataset._manifest['items'][identifier]["relpath"] == relpath:
