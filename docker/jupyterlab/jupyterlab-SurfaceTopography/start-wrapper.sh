1cc10caa4c7255c074931fc87e943bc314adcc56
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-11-10 21:43:54 +0100

    MAINT: don't delete font cache

--- docker/jupyterlab/jupyterlab-SurfaceTopography/start-wrapper.sh
+++ docker/jupyterlab/jupyterlab-SurfaceTopography/start-wrapper.sh
@@ -2,7 +2,7 @@
 set -e
 
 # delete and rebuild matplotlib font cache
-rm -rf /home/${NB_USER}/.cache/matplotlib
+# rm -rf /home/${NB_USER}/.cache/matplotlib
 fc-cache --force
 PATH=$(echo "$PATH" | sed -e 's|/opt/conda[^:]*:||g') python3 -c 'import matplotlib.font_manager; matplotlib.font_manager._load_fontmanager(try_read_cache=False)'
 
